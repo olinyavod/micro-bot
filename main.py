a124971898ba2a4818341cb9be0b42640dda3b45
olinyavod <olinyavod@gmail.com>
2023-03-05 21:37:36 +1000

    Add serial bluetooth connect

--- main.py
+++ main.py
@@ -3,32 +3,51 @@ import pyb
 import uasyncio as asyncio
 
 from robot import Robot
-from term import AConsole
+from lib.term import AConsole
+from machine import UART
+from micropython import const, alloc_emergency_exception_buf
 
 
-async def command_process(robot: Robot) -> None:
-    term = AConsole()
+async def command_process(term: AConsole, robot: Robot) -> None:
     while True:
-        cmd = await term.input("robot> ")
+        await term.print("robot> ")
+        cmd = await term.readline()
+        l = len(cmd)
+        cmd = cmd[0:l - 2] if l > 2 else cmd
 
-        if cmd == "quit":
-            return
-        elif cmd == "distance":
-            print(f"Distance: {robot.distance():.2f} cm")
+        if cmd == b'quit' or cmd == b'q':
+            raise KeyboardInterrupt
+        elif cmd == b'distance' or cmd == b'dist':
+            await term.print(f"Distance: {robot.distance():.2f} cm")
+        else:
+            print(cmd)
+
+
+async def run(robot, term):
+    await asyncio.gather(asyncio.create_task(command_process(term, robot)), asyncio.create_task(robot.run()))
 
 
 def main():
+    is_continue = False
     robot = Robot()
+    uart = UART(2, 9600)
+    uart.init(9600, bits=8, parity=None, stop=1)
 
-    loop = asyncio.get_event_loop()
-
-    loop.create_task(command_process(robot))
-    loop.create_task(robot.run())
-
-    loop.run_forever()
+    term = AConsole(uart, uart)
+    try:
+        asyncio.run(run(robot, term))
+    except KeyboardInterrupt:
+        pyb.wfi()
+        is_continue = input("Robot interrupted. Continue? y/n: ") == "y"
+    finally:
+        robot.close()
+        term.close()
+        uart.deinit()
+        asyncio.new_event_loop()
+        if is_continue:
+            main()
 
 
 if __name__ == '__main__':
-    pyb.wfi()
+    alloc_emergency_exception_buf(250)
     main()
-
--- robot.py
+++ robot.py
@@ -1,48 +1,38 @@
-import pyb
 from lib.hcsr04 import HCSR04
 import uasyncio as asyncio
-
+from lib.line_sensor import LineSensor
+from micropython import const
 
 class Robot:
 
     def __init__(self):
-        trigger_pin = pyb.Pin.cpu.B3
-        echo_pin = pyb.Pin.cpu.B4
-        self._servo = pyb.Servo(1)
-        self._dist_sensor = HCSR04(trigger_pin, echo_pin)
+        self._line_sensor = LineSensor(const("B5"), self._line_on_changed)
+        self._dist_sensor = HCSR04(const("B3"), const("B4"))
         self._dist = 0.0
 
+    def _line_on_changed(self, value:int):
+        print(f"Line: {value}")
+
     async def run(self) -> None:
-        angle_offset = const(40)
-        angle = angle_offset
-        speed = 1
-        max_angle = 0
-        max_dist = 0
         while True:
-            wait = asyncio.sleep_ms(50)
-            # self._servo.angle(angle)
-
-            # angle += speed
-
-            # if angle >= 90 or angle <= -30:
-            #  speed *= -1
-            #  angle += speed
-            #  print(f"Max {max_dist:.2f} cm on angle {max_angle-angle_offset}")
-            #  max_dist = 0
-
-            await wait
+            await asyncio.sleep_ms(50)
             has_error, dist = await self._dist_sensor.distance_cm()
 
             if has_error:
                 continue
-
             self._dist = dist
-
-            # print(f"Distance: {dist:.2f} cm")
-
-            # if max_dist < self._dist:
-            #   max_dist = self._dist
-            #  max_angle = angle
 
     def distance(self) -> float:
         return self._dist
+
+    def close(self):
+        self._line_sensor.close()
+        self._dist_sensor.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
